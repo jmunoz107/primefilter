e55b8135486bd79368aeebecdd09802415fa5379
jose munoz <josemunoz@joses-MacBook-Air.local>
2017-07-08 18:11:31 -0700

    update redirect uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "41139d6abb5c9b73e597419f90355653"
   config.secret = "85e4bae2df1312bc4864720e88a19f95"
-  config.redirect_uri = "https://primefilter.herokuapp.com/auth/shopify/callback"
+  config.redirect_uri = "<redirect_uri>"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
